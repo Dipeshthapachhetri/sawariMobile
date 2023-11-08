2d7619dcce50d26ac5887b8f1a6ce7e686508485
Dipesh Thapa Chhetri <thapadeepesh97@gmail.com>
2023-11-08 19:25:04 +0545

    change

--- layouts/home.js
+++ layouts/home.js
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react';
 import { StyleSheet, View, Image,ActivityIndicator,Text } from 'react-native';
+import { ScrollView } from 'react-native-gesture-handler';
 
 
 
@@ -8,26 +9,110 @@ const HomeScreen = ({ navigation }) => {
     
   }, [navigation]);
 
-  return (
-    <View style={styles.container}>
-      <Text>HOMES</Text>
-      <Image source={require('../assets/killer.png')} style={styles.logo} />
-    </View>
-  );
-};
 
+  return (
+   
+    <View style={{flex:1}}>
+    
+      <View style={{flex:1,backgroundColor:'red'}}>
+    <Text style={{fontSize:22,fontWeight:'700',paddingHorizontal:20, marginTop:20}}>
+    What can we help you with, dipesh?
+    </Text>
+    </View>
+    <View style={{flex:10}}>
+    <ScrollView style={{backgroundColor:'blue',borderWidth:10,paddingTop:20}} 
+       contentContainerStyle={{flexDirection:'row',flexWrap:'wrap',justifyContent:'space-around'}}>
+
+      <View style={{height:180, width:"40%",marginLeft:10, borderWidth:0.5, borderRadius:8,marginTop:20,borderColor:'#dddddd'}}>
+        <View style={{flex:1}}>
+        <View style={{flex:4, justifyContent:"center",alignItems:'center'}}>
+          <Image source={require('../assets/killer.png')}
+          style={{flex:1,width:130,height:140,resizeMode:'cover'}}/>
+        </View>
+        <View style={{flex:1,backgroundColor:'white'}}>
+      <Text style={{textAlign:'center',marginTop:2}}>Bike</Text>
+        </View>
+        </View>
+    </View>
+    {/*test*/}
+    <View style={{height:180, width:"40%",marginLeft:10, borderWidth:0.5, borderRadius:8,marginTop:20,borderColor:'#dddddd'}}>
+        <View style={{flex:1}}>
+        <View style={{flex:4, justifyContent:"center",alignItems:'center'}}>
+          <Image source={require('../assets/killer.png')}
+          style={{flex:1,width:130,height:140,resizeMode:'cover'}}/>
+        </View>
+        <View style={{flex:1,backgroundColor:'white'}}>
+      <Text style={{textAlign:'center',marginTop:2}}>Scooter</Text>
+        </View>
+        </View>
+    </View>
+    <View style={{height:180, width:"40%",marginLeft:10, borderWidth:0.5, borderRadius:8,marginTop:20,borderColor:'#dddddd'}}>
+        <View style={{flex:1}}>
+        <View style={{flex:4, justifyContent:"center",alignItems:'center'}}>
+          <Image source={require('../assets/killer.png')}
+          style={{flex:1,width:130,height:140,resizeMode:'cover'}}/>
+        </View>
+        <View style={{flex:1,backgroundColor:'white'}}>
+      <Text style={{textAlign:'center',marginTop:2}}>car</Text>
+        </View>
+        </View>
+    </View>
+    <View style={{height:180, width:"40%",marginLeft:10, borderWidth:0.5, borderRadius:8,marginTop:20,borderColor:'#dddddd'}}>
+        <View style={{flex:1}}>
+        <View style={{flex:4, justifyContent:"center",alignItems:'center'}}>
+          <Image source={require('../assets/killer.png')}
+          style={{flex:1,width:130,height:140,resizeMode:'cover'}}/>
+        </View>
+        <View style={{flex:1,backgroundColor:'white'}}>
+      <Text style={{textAlign:'center',marginTop:2}}>MiniBus/truck</Text>
+        </View>
+        </View>
+    </View>
+    <View style={{height:180, width:"40%",marginLeft:10, borderWidth:0.5, borderRadius:8,marginTop:20,borderColor:'#dddddd'}}>
+        <View style={{flex:1}}>
+        <View style={{flex:4, justifyContent:"center",alignItems:'center'}}>
+          <Image source={require('../assets/killer.png')}
+          style={{flex:1,width:130,height:140,resizeMode:'cover'}}/>
+        </View>
+        <View style={{flex:1,backgroundColor:'white'}}>
+      <Text style={{textAlign:'center',marginTop:2}}>HeavyBus/truck</Text>
+        </View>
+        </View>
+    </View>
+    <View style={{height:180, width:"40%",marginLeft:10, borderWidth:0.5, borderRadius:8,marginTop:20,borderColor:'#dddddd'}}>
+        <View style={{flex:1}}>
+        <View style={{flex:4, justifyContent:"center",alignItems:'center'}}>
+          <Image source={require('../assets/killer.png')}
+          style={{flex:1,width:130,height:140,resizeMode:'cover'}}/>
+        </View>
+        <View style={{flex:1,backgroundColor:'white'}}>
+      <Text style={{textAlign:'center',marginTop:2}}>Excavator</Text>
+        </View>
+        </View>
+    </View>
+    <View style={{height:180, width:"40%",marginLeft:10, borderWidth:0.5, borderRadius:8,marginTop:20,borderColor:'#dddddd'}}>
+        <View style={{flex:1}}>
+        <View style={{flex:4, justifyContent:"center",alignItems:'center'}}>
+          <Image source={require('../assets/killer.png')}
+          style={{flex:1,width:130,height:140,resizeMode:'cover'}}/>
+        </View>
+        <View style={{flex:1,backgroundColor:'white'}}>
+      <Text style={{textAlign:'center',marginTop:2}}>Excavator</Text>
+        </View>
+        </View>
+    </View>
+    {/*test*/}
+    </ScrollView>
+    </View>
+    </View>
+    
+
+  )}
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#fff',
-  },
-  logo: {
-    width: 120,
-    height: 120,
-    borderRadius:90
-    
   },
 });
 
