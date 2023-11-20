367fd6989d653c2886533447520d319b9a6209f7
Dipesh Thapa Chhetri <thapadeepesh97@gmail.com>
2023-11-20 15:31:30 +0545

    CHANGE

--- layouts/home.js
+++ layouts/home.js
@@ -17,87 +17,26 @@ const categories=['Bike','scooter','car','bus','excavator','micro'];
     </Text>
     </View>
     <View style={{flex:10}}>
-    <ScrollView style={{backgroundColor:'blue',}} 
-       contentContainerStyle={{flexGrow:1, flexDirection:'row',flexWrap:'wrap',justifyContent:'space-around',paddingVertical:20}}>
-      <View style={{height:180, width:"40%",marginLeft:10, borderWidth:0.5, borderRadius:8,marginTop:20,borderColor:'#dddddd'}}>
-        <View style={{flex:1}}>
-        <View style={{flex:4, justifyContent:"center",alignItems:'center'}}>
-          <Image source={require('../assets/killer.png')}
-          style={{flex:1,width:130,height:140,resizeMode:'cover'}}/>
-        </View>
-        <View style={{flex:1,backgroundColor:'white'}}>
-      <Text style={{textAlign:'center',marginTop:2}}>Bike</Text>
-        </View>
-        </View>
-    </View>
-    {/*test*/}
-    <View style={{height:180, width:"40%",marginLeft:10, borderWidth:0.5, borderRadius:8,marginTop:20,borderColor:'#dddddd'}}>
-        <View style={{flex:1}}>
-        <View style={{flex:4, justifyContent:"center",alignItems:'center'}}>
-          <Image source={require('../assets/killer.png')}
-          style={{flex:1,width:130,height:140,resizeMode:'cover'}}/>
-        </View>
-        <View style={{flex:1,backgroundColor:'white'}}>
-      <Text style={{textAlign:'center',marginTop:2}}>Scooter</Text>
-        </View>
-        </View>
-    </View>
-    <View style={{height:180, width:"40%",marginLeft:10, borderWidth:0.5, borderRadius:8,marginTop:20,borderColor:'#dddddd'}}>
-        <View style={{flex:1}}>
-        <View style={{flex:4, justifyContent:"center",alignItems:'center'}}>
-          <Image source={require('../assets/killer.png')}
-          style={{flex:1,width:130,height:140,resizeMode:'cover'}}/>
-        </View>
-        <View style={{flex:1,backgroundColor:'white'}}>
-      <Text style={{textAlign:'center',marginTop:2}}>car</Text>
-        </View>
-        </View>
-    </View>
-    <View style={{height:180, width:"40%",marginLeft:10, borderWidth:0.5, borderRadius:8,marginTop:20,borderColor:'#dddddd'}}>
-        <View style={{flex:1}}>
-        <View style={{flex:4, justifyContent:"center",alignItems:'center'}}>
-          <Image source={require('../assets/killer.png')}
-          style={{flex:1,width:130,height:140,resizeMode:'cover'}}/>
-        </View>
-        <View style={{flex:1,backgroundColor:'white'}}>
-      <Text style={{textAlign:'center',marginTop:2}}>MiniBus/truck</Text>
-        </View>
-        </View>
-    </View>
-    <View style={{height:180, width:"40%",marginLeft:10, borderWidth:0.5, borderRadius:8,marginTop:20,borderColor:'#dddddd'}}>
-        <View style={{flex:1}}>
-        <View style={{flex:4, justifyContent:"center",alignItems:'center'}}>
-          <Image source={require('../assets/killer.png')}
-          style={{flex:1,width:130,height:140,resizeMode:'cover'}}/>
-        </View>
-        <View style={{flex:1,backgroundColor:'white'}}>
-      <Text style={{textAlign:'center',marginTop:2}}>HeavyBus/truck</Text>
-        </View>
-        </View>
-    </View>
-    <View style={{height:180, width:"40%",marginLeft:10, borderWidth:0.5, borderRadius:8,marginTop:20,borderColor:'#dddddd'}}>
-        <View style={{flex:1}}>
-        <View style={{flex:4, justifyContent:"center",alignItems:'center'}}>
-          <Image source={require('../assets/killer.png')}
-          style={{flex:1,width:130,height:140,resizeMode:'cover'}}/>
-        </View>
-        <View style={{flex:1,backgroundColor:'white'}}>
-      <Text style={{textAlign:'center',marginTop:2}}>Excavator</Text>
-        </View>
-        </View>
-    </View>
-    <View style={{height:180, width:"40%",marginLeft:10, borderWidth:0.5, borderRadius:8,marginTop:20,borderColor:'#dddddd'}}>
-        <View style={{flex:1}}>
-        <View style={{flex:4, justifyContent:"center",alignItems:'center'}}>
-          <Image source={require('../assets/killer.png')}
-          style={{flex:1,width:130,height:140,resizeMode:'cover'}}/>
-        </View>
-        <View style={{flex:1,backgroundColor:'white'}}>
-      <Text style={{textAlign:'center',marginTop:2}}>Excavator</Text>
-        </View>
-        </View>
-    </View>
-    {/*test*/}
+    <ScrollView style={{backgroundColor:'white',borderWidth:2,paddingTop:20,borderColor:'grey'}} 
+       contentContainerStyle={{flexDirection:'row',flexWrap:'wrap',justifyContent:'space-around'}}>
+        {
+          categories.map(category=>{
+            return( 
+            <TouchableOpacity key={category} onPress={() => navigation.navigate("Opt")} style={{height:180, width:"40%",marginLeft:10, borderWidth:0.5, borderRadius:8,marginTop:20,borderColor:'#dddddd'}}>
+            <View style={{flex:1}}>
+            <View style={{flex:4, justifyContent:"center",alignItems:'center',borderColor:'grey',borderWidth:2,borderRadius:10}}>
+              <Image source={require('../assets/killer.png')}
+              style={{flex:1,width:130,height:140,resizeMode:'cover'}}/>
+            </View>
+            <View style={{flex:1,backgroundColor:'white',borderColor:'grey',borderWidth:2,borderRadius:10}}>
+          <Text style={{textAlign:'center',marginTop:2}}>{category}</Text>
+            </View>
+            </View>
+          </TouchableOpacity>)
+
+          })    
+}
+   
     </ScrollView>
     </View>
     </View>
