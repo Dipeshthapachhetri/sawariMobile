0012a525d4f2a5b50777e826cb558a7cc3742729
Samikshya <samikshyarana987@gmail.com>
2024-01-21 14:50:16 +0545

    changes

--- layouts/question.js
+++ layouts/question.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 import { StyleSheet, View, Image,ActivityIndicator,Text,Button } from 'react-native';
 import { useRoute } from '@react-navigation/native';
+import { ScrollView } from 'react-native-gesture-handler';
 
 const QuestionScreen = ({ navigation }) => {
 
@@ -8,6 +9,7 @@ const QuestionScreen = ({ navigation }) => {
    const route=useRoute()
    console.log(route.params.category);
    console.log(route.params.questions);
+let quest=(route.params.questions);
    
   return (
     <View style={{flex:1}}>
@@ -17,7 +19,22 @@ const QuestionScreen = ({ navigation }) => {
     </Text>
     </View>
     <View style={styles.container}>
-      <Image source={require('../assets/killer.png')} style={styles.logo} />
+    <ScrollView>
+     {quest.map(question=>{
+        return(
+      
+        <View style={{margin:10}}>
+         <Text>{question.number}.{question.question}</Text>
+         {[0,1,2,3].map(index=>{
+          let correct= question.options[index]== question.correct;
+          return(
+            
+         <Text style={{backgroundColor:correct?'green':'white'}}>{index+1}.{question.options[index]}</Text>
+     )})}
+        </View>
+      )
+      })}
+      </ScrollView>
     </View>
     <View style={{flex:1, flexDirection:'row',backgroundColor:'#5ce1e6',justifyContent:'center',alignItems:'center'}}>
     <Button title="Back"onPress={() => navigation.navigate('Navigator')}/>
