c631b635b3b555f44c42597f20c1d8c51b77a799
Dipesh Thapa Chhetri <thapadeepesh97@gmail.com>
2024-01-27 22:40:17 +0545

    questioncall

--- layouts/mocktest.js
+++ layouts/mocktest.js
@@ -1,9 +1,10 @@
 import React, { useEffect } from 'react';
 import { StyleSheet, View, Image,ActivityIndicator,Text,Button } from 'react-native';
 import { useRoute } from '@react-navigation/native';
+import {useSelector} from 'react-redux';
 
 const MockScreen = ({ navigation }) => {
-
+  const user=useSelector(state=>state.user.userDetail);
 
    const route=useRoute()
    console.log(route.params.category);
@@ -14,7 +15,7 @@ const MockScreen = ({ navigation }) => {
     <View style={{flex:1}}>
     <View style={{flex:1,backgroundColor:'#5ce1e6'}}>
     <Text style={{fontSize:22,fontWeight:'700',paddingHorizontal:20, marginTop:20}}>
-    Ready for the Mocktest, Shiva?
+    Ready for the Mocktest, {user.username}?
     </Text>
     </View>
 
